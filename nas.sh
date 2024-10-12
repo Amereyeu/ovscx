e590cf9e0e13946a99794009633a0f5c2f341b63
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-10-12 15:14:11 +0200

    Update nas.sh

--- nas.sh
+++ nas.sh
@@ -1,1 +1,117 @@
 
+#!/bin/bash
+
+# colors
+switch="\033["
+normal="${switch}0m"
+color="${switch}1;35m"
+
+
+
+echo
+echo -e "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ${color}Checking for updates...${normal} "
+echo
+sudo apt update && sudo apt upgrade -y
+sudo apt autoremove -y
+
+
+
+echo
+echo -e "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ${color}Installing Curl...${normal} "
+echo
+sudo apt install curl 
+
+
+
+echo
+echo -e "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ${color}Installing Git...${normal} "
+echo
+sudo apt install git -y
+git --version
+
+
+
+echo
+echo -e "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ${color}Installing ZSH Shell...${normal} "
+echo
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+chsh -s $(which zsh)
+
+
+
+echo
+echo -e "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ${color}Installing powerlevel10k ohmyzsh theme...${normal} "
+echo
+sudo apt-get install fonts-powerline -y
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+
+echo
+echo -e "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ${color}Installing Nala package manager...${normal} "
+echo
+sudo apt install nala -y
+
+
+
+echo
+echo -e "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ${color}Installing Midnight commander...${normal} "
+echo
+sudo apt install mc -y
+
+
+
+echo
+echo -e "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ${color}Installing Btop++...${normal} "
+echo
+sudo apt install btop -y
+
+
+
+echo
+echo -e "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ${color}Installing Samba...${normal} "
+echo
+sudo apt install samba samba-common-bin -y
+
+
+
+
+
+cp /media/USBHDD/backups/scripts/.zshrc ~
+
+cp /media/USBHDD/backups/scripts/.p10k.zsh ~
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
